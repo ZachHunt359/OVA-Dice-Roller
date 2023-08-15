514144051617be701b79b0036aad14c132fe4065
Zach Hunt <56412983+ZachHunt359@users.noreply.github.com>
2023-08-15 14:58:44 -0700

    Update OVADiceRoller.js
    Switching from a textarea to a div so I can format the displayed text, and using a temporary textarea during copyTextToClipboard() to strip that formatting out of the copied text.

--- js/OVADiceRoller.js
+++ js/OVADiceRoller.js
@@ -1,78 +1,98 @@
 function rollDice() {
     const bonusesInput = document.getElementById("bonuses");
     const numDiceInput = document.getElementById("numDice");
-    const resultInput = document.getElementById("result");
-
+    const resultDiv = document.getElementById("resultDiv");
+  
     let numBonus = parseInt(bonusesInput.value);
     let numDice = parseInt(numDiceInput.textContent);
     let isNegative = (numBonus < 0); //Are we doing a 'Negative Dice' roll?
     let total = 0;
     let resultText = "";
     const rolls = [];
-
+  
     for (let i = 0; i < numDice; i++) {
-        const roll = Math.floor(Math.random() * 6) + 1; // Simulating a 6-sided dice
-        rolls.push(roll);
+      const roll = Math.floor(Math.random() * 6) + 1; // Simulating a 6-sided dice
+      rolls.push(roll);
     }
-
+  
     rolls.sort(); // Sort the rolls array
     rolls.reverse(); // Reverse that sort, so it's in descending order
-
+  
     const rollCounts = {};
     for (const roll of rolls) {
-        if (rollCounts[roll]) {
-            if (!isNegative) {
-                rollCounts[roll] += roll;
-            } else {
-                rollCounts[roll] = roll; //Duplicate dice are never added together when rolling 'Negative Dice'
-            }
-
+      if (rollCounts[roll]) {
+        if (!isNegative) {
+          rollCounts[roll] += roll;
         } else {
-            rollCounts[roll] = roll;
+          rollCounts[roll] = roll; //Duplicate dice are never added together when rolling 'Negative Dice'
         }
+  
+      } else {
+        rollCounts[roll] = roll;
+      }
     }
-
+  
     let highestValue = 0;
     let lowestValue = Infinity;
-
+    let highlightDice = 0;
+  
     for (const [roll, value] of Object.entries(rollCounts)) {
-        if (value > highestValue) {
-            highestValue = value;
-        }
-        if (value < lowestValue) {
-            lowestValue = value;
-        }
+      if (!isNegative && value > highestValue) {
+        highestValue = value;
+        highlightDice = roll;
+      }
+      if (isNegative && value < lowestValue) {
+        lowestValue = value;
+        highlightDice = roll;
+      }
     }
     if (!isNegative) {
-        resultText = `Total: ${highestValue}` + "\n\n";
+      resultText = `Total: ${highestValue}` + "\n\n";
     } else {
-        resultText = `Total (Lowest): ${lowestValue}` + "\n\n";
+      resultText = `Total (Lowest): ${lowestValue}` + "\n\n";
     }
-
+  
     resultText += "Rolls: " + rolls.join(", ");
-    resultInput.value = resultText;
-	
-	// Add click event listener to the copy button
+    resultDiv.textContent = resultText;
+    winningDice(highlightDice, resultDiv);
+  
+    // Add click event listener to the copy button
     const copyButton = document.getElementById("copyButton");
     copyButton.addEventListener("click", function() {
-        copyTextToClipboard(resultInput);
+      copyTextToClipboard(resultDiv);
     });
-}
-
-function updateNumDice() {
+  }
+  
+  function updateNumDice() {
     const bonusesInput = document.getElementById("bonuses");
     const numDiceInput = document.getElementById("numDice");
-
+  
     const bonuses = parseInt(bonusesInput.value);
     let numDice = bonuses + 2;
     if (bonuses < 0) {
-        numDice = Math.abs(bonuses)
+      numDice = Math.abs(bonuses)
     }
     numDiceInput.textContent = numDice;
-}
-// Function to copy text to the clipboard
-function copyTextToClipboard(element) {
-    element.select();
+  }
+  // Function to copy text to the clipboard
+  function copyTextToClipboard(element) {
+    const plainText = element.innerText;
+  
+    const textArea = document.createElement("textarea");
+    textArea.value = plainText;
+    document.body.appendChild(textArea);
+  
+    textArea.select();
     document.execCommand("copy");
-    window.getSelection().removeAllRanges(); // Clear the selection
-}
+  
+    document.body.removeChild(textArea);  
+  }
+  
+  function winningDice(rolledNum, element) {
+    const regex = new RegExp(`(?<=Rolls:.*)(\\b${rolledNum}\\b)`, 'g');
+    const originalContent = element.innerHTML;
+    const modifiedContent = originalContent.replace(regex, `<span class="winningDice">${rolledNum}</span>`);
+  
+    element.innerHTML = modifiedContent;
+  }
+  
